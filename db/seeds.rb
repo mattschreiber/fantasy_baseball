9ae79b9fdbd9be7d16ed5d675395233d9228b963
mattschreiber <matt.schreiber01@gmail.com>
2016-07-13 12:49:52 -0400

    added owners.json file to seed owners table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Owner.destroy_all
 
-fn = ["Matt", "Scott", "Alan", "Mike", "Pete", "Mike", "Jay", "James", "Joe", "David", "James","", "Mike", "Mike", "Brian", "Scott"]
-ln = ["Schreiber", "Pierce", "Thomay", "Thellman", "Tran", "Kelly", "Zadzilka",
-			"Ungerer", "Shaw", "Thompson", "Wailani","", "McNamara", "Hoendorf", "Sweet", "Reifschneider"]
-tn = ["Pink Unicorns", "Silver Sluggers", "Morgantown Swingers", "Championship Vinyl",
-			"Rockford Peaches", "Shiva Bastards", "Brunswick Bulldozers", "Fluffy White Bunnies",
-			"The Florida Dumpster Fire", "Greenbriar Sycamores", "", "Alderaan Asteroid","", "", "", ""]
+# fn = ["Matt", "Scott", "Alan", "Mike", "Pete", "Mike", "Jay", "James", "Joe", "David", "James","", "Mike", "Mike", "Brian", "Scott"]
+# ln = ["Schreiber", "Pierce", "Thomay", "Thellman", "Tran", "Kelly", "Zadzilka",
+# 			"Ungerer", "Shaw", "Thompson", "Wailani","", "McNamara", "Hoendorf", "Sweet", "Reifschneider"]
+# tn = ["Pink Unicorns", "Silver Sluggers", "Morgantown Swingers", "Championship Vinyl",
+# 			"Rockford Peaches", "Shiva Bastards", "Brunswick Bulldozers", "Fluffy White Bunnies",
+# 			"The Florida Dumpster Fire", "Greenbriar Sycamores", "", "Alderaan Asteroid","", "", "", ""]
+# id = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-# Owner.create(first_name: "Matt", last_name: "Schreiber", team_name: "Pink Unicorns")
+# # Owner.create(first_name: "Matt", last_name: "Schreiber", team_name: "Pink Unicorns")
 
-10.times do |n|
-	Owner.create!(first_name: fn[n], last_name: ln[n], team_name: tn[n])
+# fn.length.times do |n|
+# 	Owner.create!(id: id[n], first_name: fn[n], last_name: ln[n], team_name: tn[n])
+# end
+
+file = File.read('spec/models/owners.json')
+data_hash = JSON.parse(file)
+data_hash.each do |r|
+  Owner.create!(r)
 end
