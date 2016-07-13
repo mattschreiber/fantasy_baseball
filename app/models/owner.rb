fabc6e975112b3cdd830c501bb7edcbbfa4089d9
mattschreiber <matt.schreiber01@gmail.com>
2016-07-13 12:51:31 -0400

    added calc_team_averages method

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,10 +1,10 @@
 class Owner < ActiveRecord::Base
-	has_many :team_seasons, dependent: :destroy
+	has_many :team_seasons
 
-	# after_find do |owner|
- #    @avg_place = owner.team_seasons.average(:place).to_s
- #    @num_titles = owner.team_seasons.where(place: 1).count
- #  end
+	attr_reader :place_avg, :total_points_avg, :num_titles
+	after_find do |owner|
+    calc_team_averages
+  end
 
 	def num_titles
 		self.team_seasons.where(place: 1).count
@@ -14,4 +14,22 @@ class Owner < ActiveRecord::Base
 	def calc_avg(category)
 		self.team_seasons.average(:"#{category}").round(2).to_s
 	end
+
+private
+	def calc_team_averages
+		@num_titles = []
+		if self.team_seasons.exists?
+			self.team_seasons.each do |season|
+				@place_avg = season[:place] + (@place_avg.nil? ? 0 : @place_avg)
+				@total_points_avg = season[:total_points] + (@total_points_avg.nil? ? 0 : @total_points_avg)
+				@num_titles << season[:place]
+			end
+			count = self.team_seasons.length
+			@place_avg = (@place_avg / count.to_f).round(2)
+			@total_points_avg = (@total_points_avg /count.to_f).round(2)
+			@num_titles = @num_titles.select {|title| title == 1}.count
+	end
+	end
 end
+
+#  col_list.each {|col| pp hash[:"#{col}"] = owner.calc_avg("#{col}") }
