b11614722b03d31a6edd55a7cd3a791b4e5d7a44
mattschreiber <matt.schreiber01@gmail.com>
2016-07-15 11:26:48 -0400

    changed name to singular to match ruby convention

--- app/models/category_average.rb
+++ app/models/category_average.rb
@@ -1,7 +1,7 @@
 # This class will serve to calculate category averages such as the average number of 
 # homeruns per team per season.
 
-class CategoryAverages
+class CategoryAverage
 
 	attr_reader :run_avg, :hr_avg, :rbi_avg, :sb_avg, :batting_avg, :win_avg, :k_avg,
 							:sv_avg, :era_avg, :whip_avg
@@ -15,8 +15,22 @@ class CategoryAverages
 		TeamSeason.average(:"#{category}").round(2).to_s
 	end
 
+	def self.hash_avg
+		h = {}
+		array = []
+		TeamSeason.all.each do |season|
+			TeamSeason.column_names.slice(1..-3).each {|r| h["#{r}"] = season[:"#{r}"]}
+			array << h
+			h = {}
+		end
+		return array
+
+		# hr_avg = arr.map{|a| a["total_hr"]}.reduce(:+)/arr.length
+	end
+
 	#only calculates averages for actual totals not point categories
 	def category_averages
+		#calculate totals for each category
 		TeamSeason.all.each do |season|
 			@run_avg = season.total_run + (@run_avg.nil? ? 0 : @run_avg)
 			@hr_avg = season.total_hr + (@hr_avg.nil? ? 0: @hr_avg)
@@ -30,6 +44,7 @@ class CategoryAverages
 			@era_avg = season.total_era + (@era_avg.nil? ? 0 : @era_avg)
 			@whip_avg = season.total_whip + (@whip_avg.nil? ? 0 : @whip_avg)
 		end
+		# calculate average
 		count = TeamSeason.count
 		@run_avg = @run_avg / count.to_f
 		@hr_avg = @hr_avg / count.to_f
