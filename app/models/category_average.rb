96b2d67bc9ad62dee1b10e479e8d985aef51b196
mattschreiber <matt.schreiber01@gmail.com>
2016-07-15 13:20:36 -0400

    created class method to return a hash of category averages. The initialize method returns an object with category averages set

--- app/models/category_average.rb
+++ app/models/category_average.rb
@@ -3,60 +3,57 @@
 
 class CategoryAverage
 
-	attr_reader :run_avg, :hr_avg, :rbi_avg, :sb_avg, :batting_avg, :win_avg, :k_avg,
-							:sv_avg, :era_avg, :whip_avg
+	attr_reader :total_run_avg, :total_hr_avg, :total_rbi_avg, :total_sb_avg, :total_avg_avg, :total_win_avg, :total_k_avg,
+							:total_sv_avg, :total_era_avg, :total_whip_avg
 
 	
 	def initialize 
-		category_averages
+		# category_averages
+		self.class.calc_all
 	end
 
-	def self.calc_avg(category)
-		TeamSeason.average(:"#{category}").round(2).to_s
+	def self.calc_one(category)
+		TeamSeason.average(:"#{category}").to_s
 	end
 
-	def self.hash_avg
-		h = {}
-		array = []
-		TeamSeason.all.each do |season|
-			TeamSeason.column_names.slice(1..-3).each {|r| h["#{r}"] = season[:"#{r}"]}
-			array << h
-			h = {}
+	def self.calc_all
+		#class method that returns hash of category averages. hash keys match class attributes
+		hash = {}
+		TeamSeason.column_names.each do |name|
+			hash[:"#{name}_avg"] = calc_one(name) unless TeamSeason.column_for_attribute(name).type == :datetime
 		end
-		return array
-
-		# hr_avg = arr.map{|a| a["total_hr"]}.reduce(:+)/arr.length
+		return hash
 	end
 
 	#only calculates averages for actual totals not point categories
 	def category_averages
 		#calculate totals for each category
 		TeamSeason.all.each do |season|
-			@run_avg = season.total_run + (@run_avg.nil? ? 0 : @run_avg)
-			@hr_avg = season.total_hr + (@hr_avg.nil? ? 0: @hr_avg)
-			@rbi_avg = season.total_rbi + (@rbi_avg.nil? ? 0 : @rbi_avg)
-			@sb_avg = season.total_sb + (@sb_avg.nil? ? 0 : @sb_avg)
-			@batting_avg = season.total_avg + (@batting_avg.nil? ? 0 : @batting_avg)
+			@total_run_avg = season.total_run + (@total_run_avg.nil? ? 0 : @total_run_avg)
+			@total_hr_avg = season.total_hr + (@total_hr_avg.nil? ? 0: @total_hr_avg)
+			@total_rbi_avg = season.total_rbi + (@total_rbi_avg.nil? ? 0 : @total_rbi_avg)
+			@total_sb_avg = season.total_sb + (@total_sb_avg.nil? ? 0 : @total_sb_avg)
+			@total_avg_avg = season.total_avg + (@total_avg_avg.nil? ? 0 : @total_avg_avg)
 
-			@win_avg = season.total_win + (@win_avg.nil? ? 0 : @win_avg)
-			@k_avg = season.total_k + (@k_avg.nil? ? 0 : @k_avg)
-			@sv_avg = season.total_sv + (@sv_avg.nil? ? 0 : @sv_avg)
-			@era_avg = season.total_era + (@era_avg.nil? ? 0 : @era_avg)
-			@whip_avg = season.total_whip + (@whip_avg.nil? ? 0 : @whip_avg)
+			@total_win_avg = season.total_win + (@total_win_avg.nil? ? 0 : @total_win_avg)
+			@total_k_avg = season.total_k + (@total_k_avg.nil? ? 0 : @total_k_avg)
+			@total_sv_avg = season.total_sv + (@total_sv_avg.nil? ? 0 : @total_sv_avg)
+			@total_era_avg = season.total_era + (@total_era_avg.nil? ? 0 : @total_era_avg)
+			@total_whip_avg = season.total_whip + (@total_whip_avg.nil? ? 0 : @total_whip_avg)
 		end
 		# calculate average
 		count = TeamSeason.count
-		@run_avg = @run_avg / count.to_f
-		@hr_avg = @hr_avg / count.to_f
-		@rbi_avg = @rbi_avg / count.to_f
-		@sb_avg = @sb_avg / count.to_f
-		@batting_avg = @batting_avg / count.to_f
+		@total_run_avg = @total_run_avg / count.to_f
+		@total_hr_avg = @total_hr_avg / count.to_f
+		@total_rbi_avg = @total_rbi_avg / count.to_f
+		@total_sb_avg = @total_sb_avg / count.to_f
+		@total_avg_avg = @total_avg_avg / count.to_f
 
-		@win_avg = @win_avg / count.to_f
-		@k_avg = @k_avg / count.to_f
-		@sv_avg = @sv_avg / count.to_f
-		@era_avg = @era_avg / count.to_f
-		@whip_avg = @whip_avg / count.to_f
+		@total_win_avg = @total_win_avg / count.to_f
+		@total_k_avg = @total_k_avg / count.to_f
+		@total_sv_avg = @total_sv_avg / count.to_f
+		@total_era_avg = @total_era_avg / count.to_f
+		@total_whip_avg = @total_whip_avg / count.to_f
 	end
 
 end
