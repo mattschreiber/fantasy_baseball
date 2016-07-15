a0aa3f70413ba01cebf6e57cd6d7933fa9fefbe8
mattschreiber <matt.schreiber01@gmail.com>
2016-07-15 14:45:38 -0400

    added comments explaining how methods of CategoryAverage class work

--- app/models/category_average.rb
+++ app/models/category_average.rb
@@ -1,12 +1,12 @@
-# This class will serve to calculate category averages such as the average number of 
+# This class will serve to calculate category averages used typically for generating reports such as the average number of 
 # homeruns per team per season.
 
 class CategoryAverage
 
-	# CATEGORIES = ["total_run_avg", "total_hr_avg", "total_rbi_avg", "total_sb_avg", 
-	# 	"total_avg_avg", "total_win_avg", "total_k_avg", "total_sv_avg", "total_era_avg", "total_whip_avg"]
 
 # dynamically define methods that return category averages
+# Loop throught the team_seasons table and use the column names to call the calc_one method
+# which returns the average for the column (aka category). 
 	TeamSeason.column_names.each do |cat|
 		define_method("#{cat}_avg") do
 			self.class.calc_one("#{cat}")
@@ -25,7 +25,5 @@ class CategoryAverage
 		end
 		return hash
 	end
-
-
 
 end
