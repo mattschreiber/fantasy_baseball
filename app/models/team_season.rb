971d9f129866c9f0814940f27904be5b248154d3
mattschreiber <matt.schreiber01@gmail.com>
2016-07-17 13:24:22 -0400

    sketching sort algorithm to  calculate teams point scores

--- app/models/team_season.rb
+++ app/models/team_season.rb
@@ -3,4 +3,25 @@ class TeamSeason < ActiveRecord::Base
 
 	default_scope {order year: :desc}
 
+
+
+
+def self.calc_season_points
+	
+	hash = {}
+	sorted_hash = {}
+	h= {}
+	hash = TeamSeason.where(year: 2015).pluck(:owner_id, :total_hr).to_h
+	sorted_hash = hash.sort_by{|owner, run| run}.reverse.to_h
+	
+	points = 10
+	sorted_hash.keys.each do |k|
+		h["#{k}"]= points
+	points = points - 1
+	
+	end
+	return h
+end
+
+
 end
