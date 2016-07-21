5079bf42adb66598139568a904f85c1a4234b9d4
mattschreiber <matt.schreiber01@gmail.com>
2016-07-21 09:15:58 -0400

    added columns to current_season:boolean, gp:integer, innings:float to team_seasons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713161644) do
+ActiveRecord::Schema.define(version: 20160721121800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,11 @@ ActiveRecord::Schema.define(version: 20160713161644) do
     t.float    "total_era"
     t.float    "total_points"
     t.integer  "owner_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.boolean  "current_season"
+    t.float    "innings"
+    t.integer  "gp"
   end
 
   add_index "team_seasons", ["owner_id"], name: "index_team_seasons_on_owner_id", using: :btree
