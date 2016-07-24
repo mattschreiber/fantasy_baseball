d9c5fc6097552b1abe08879057400dab59f06a3f
mattschreiber <matt.schreiber01@gmail.com>
2016-07-24 11:59:36 -0400

    added action for historical league view

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -4,6 +4,12 @@ class SeasonsController < ApplicationController
 		@current_seasons = CurrentSeason.ratio_standings
 	end
 
+	def historical
+		@category_average = CategoryAverage.new
+		# render json: "soon to be my page"
+		# render :template => 'seasons/historical'
+	end
+
 	def import
 		TeamSeason.import(params[:file], params[:current])
 		redirect_to seasons_url, notice: "Current Season imported"
