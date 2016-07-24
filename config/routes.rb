d4a5dd63ef3a59d1b25b504007d4b8ca49845cde
mattschreiber <matt.schreiber01@gmail.com>
2016-07-24 11:59:04 -0400

    added route for league historical data

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :owners
   get 'seasons', to: 'seasons#index'
   get 'seasons/new' 
+  get 'historical', to: 'seasons#historical'
   post 'seasons/import', to: 'seasons#import'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
