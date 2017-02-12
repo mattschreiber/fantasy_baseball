b8264edf8540dde10ea8f02795d4568a82b401ab
mattschreiber <matt.schreiber01@gmail.com>
2017-02-12 13:06:39 -0500

    updated stats controller to handle if bat or pitch is selected and position isn't correct

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,22 @@
 class StatsController < ApplicationController
   def index
   	if params[:pos]
-  		@players = Position.search(params[:pos], params[:q])
-      respond_to do |format|
+      if (params[:pos] == 'sp' || params[:pos] == 'rp') and params[:bat_pitch] == "true"
+        @players = Player.search(params[:q], params[:bat_pitch])
+      elsif (params[:pos] == 'c' || params[:pos] == '1b' || params[:pos] == '2b' || params[:pos] == '3b' || params[:pos] == 'ss' || params[:pos] == 'of') and params[:bat_pitch] == 'false'
+        @players = Player.search(params[:q], params[:bat_pitch])
+      else
+        @players = Position.search(params[:pos], params[:q])
+      end
+  		# 
+      if params[:bat_pitch] != "false"
+        respond_to do |format|
           format.html { render :batter }
+        end
+      else
+        respond_to do |format|
+          format.html {render :pitcher}
+        end
       end
   	else params[:q]
   		@players = Player.search(params[:q], params[:bat_pitch])
