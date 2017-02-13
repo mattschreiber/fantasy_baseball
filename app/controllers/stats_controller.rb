b7c1e781b4f45166c105074e1c32356afb730115
mattschreiber <matt.schreiber01@gmail.com>
2017-02-12 20:44:38 -0500

    updated stat's controller plus batter and pitcher views

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,23 +1,30 @@
 class StatsController < ApplicationController
   def index
   	if params[:pos]
+      #hack if someone enters Bat incorrect position
       if (params[:pos] == 'sp' || params[:pos] == 'rp') and params[:bat_pitch] == "true"
         @players = Player.search(params[:q], params[:bat_pitch])
+      #hack if someone enters Pitch with an incorrect position
       elsif (params[:pos] == 'c' || params[:pos] == '1b' || params[:pos] == '2b' || params[:pos] == '3b' || params[:pos] == 'ss' || params[:pos] == 'of') and params[:bat_pitch] == 'false'
         @players = Player.search(params[:q], params[:bat_pitch])
+      elsif params[:pos] == 'a'
+        @players = Player.search(params[:q], params[:bat_pitch])
       else
+        # search by position    
         @players = Position.search(params[:pos], params[:q])
       end
-  		# 
+  		# render batters 
       if params[:bat_pitch] != "false"
         respond_to do |format|
           format.html { render :batter }
         end
+      #render pitchers 
       else
         respond_to do |format|
           format.html {render :pitcher}
         end
       end
+    # search by player name without a position 
   	else params[:q]
   		@players = Player.search(params[:q], params[:bat_pitch])
       if params[:bat_pitch] != "false"
