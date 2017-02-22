c2772bd6b5f85e9e3b1958f3cd6f0c01160a313a
mattschreiber <matt.schreiber01@gmail.com>
2017-02-21 21:28:31 -0500

    Fixed broken position search along with styling Owner show page

--- app/models/position.rb
+++ app/models/position.rb
@@ -3,9 +3,9 @@ class Position < ActiveRecord::Base
 
 	def self.search(position, last_name, year, bat_pitch)
 		if bat_pitch == "true"
-  	players = find_by("pos = ?", position).players.includes(:player_ranking, :battings).where("last_name LIKE ? AND battings.year = ?", "%#{last_name}%", year).order("player_rankings.espn")
+  	players = find_by("pos = ?", position.upcase).players.includes(:player_ranking, :battings).where("last_name LIKE ? AND battings.year = ?", "%#{last_name}%", year).order("player_rankings.espn")
   else
-  	players = find_by("pos = ?", position).players.includes(:player_ranking, :pitchings).where("last_name LIKE ? AND pitchings.year = ?", "%#{last_name}%", year).order("player_rankings.espn")
+  	players = find_by("pos = ?", position.upcase).players.includes(:player_ranking, :pitchings).where("last_name LIKE ? AND pitchings.year = ?", "%#{last_name}%", year).order("player_rankings.espn")
   end
 	end
 end
