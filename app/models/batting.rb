7546385e14fddf078e5a687434e2cff50b088fa9
mattschreiber <matt.schreiber01@gmail.com>
2017-02-22 21:30:26 -0500

    added method to batter to return totals for each category based on year by team. Plus added spinner to be displayed when ajax is reloading stats

--- app/models/batting.rb
+++ app/models/batting.rb
@@ -1,4 +1,19 @@
 class Batting < ActiveRecord::Base
   belongs_to :player
   belongs_to :mlbteam
+
+  def self.team_totals(year, owner_id)
+  	#calculates a teams totals for each category
+  	batters = self.joins(player: :owner).where("year = ? AND players.owner_id = ?", year, owner_id).as_json
+  	total = Hash.new(0)
+  	batters.each do |batter|
+  		batter.slice!("runs", "hr", "rbi", "sb", "average")
+  		batter.each do |k, v|
+  			total[k] += v
+  		end
+  	end
+  	total["average"] = total["average"] / batters.count
+  	return total
+  end
+
 end
