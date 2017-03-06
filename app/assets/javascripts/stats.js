90ad49ec1f4485fd5536a2fefc6df8a0e3ae394d
mattschreiber <matt.schreiber01@gmail.com>
2017-03-06 11:13:45 -0500

    added logic to gather player_id with goal to add the ability to compare multiple players

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -4,7 +4,33 @@ $(document).on('turbolinks:load', function() {
    	 sortList: [[1,0]],
      sortRestart : true,
      sortInitialOrder: 'desc',
-   });  
+   });
+
+   var playerArray = [];
+
+
+  $('#search-submit').on('click', function(){
+    playerArray = [];
+  });
+
+  $('#stats').on('click', '.compare-cb', function(event) {
+
+    // Your normal onclick code
+    // $(".compare-cb").on('change', function(){
+      if (this.checked == true) {
+        playerArray.push(this.value);
+      } else {
+        var index = playerArray.indexOf(this.value);
+        if (index > -1) {
+          playerArray.splice(index, 1);
+        }
+      } // end if/else
+      alert(playerArray);
+  }); //end compare-cb.on change function
+
+
+
+
 });
 
  function render_partial() {
