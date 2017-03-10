ad90809703d576b82f3e7ad9b468d8ecd6367f9a
mattschreiber <matt.schreiber01@gmail.com>
2017-03-10 13:16:07 -0500

    started working through how to dynamically add notes for player

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -27,6 +27,28 @@ $("#player_owner_id").on('change', function() {
 	}
 });
 
+$("#add-note-btn").on('click', function(e) {
+  e.preventDefault();
+
+  // ajax post method to create new note with data{player_id and owner_id}
+
+  alert($('.duplicatable_nested_form').length);
+
+  newNestedForm = '<input class="form-control duplicatable_nested_form"' +
+  ' type="text" value="" name="player[notes_attributes][2][note]" id="player_notes_attributes_2_note">' +
+  '<input type="hidden" value="5" name="player[notes_attributes][2][id]" id="player_notes_attributes_2_id">'
+
+
+
+  //
+
+
+  // nestedForm = $('.duplicatable_nested_form').last().clone();
+  lastNestedForm = $('.duplicatable_nested_form').last();
+  // newNestedForm  = $(nestedForm).clone();
+  $( newNestedForm ).insertAfter( lastNestedForm );
+});
+
 // $("#update-submit").on('click', function(){
 // //
 //     var note = $("textarea").val();
