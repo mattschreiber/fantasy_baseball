096b12c0967315b2b34be580ccae72dd9e0c4436
mattschreiber <matt.schreiber01@gmail.com>
2017-03-10 14:24:29 -0500

    add logic towards be able to dynamically add notes to player

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -32,21 +32,41 @@ $("#add-note-btn").on('click', function(e) {
 
   // ajax post method to create new note with data{player_id and owner_id}
 
-  alert($('.duplicatable_nested_form').length);
+  player_id = $(this).attr("data-ownerid");
+  owner_id = $('#player_owner_id').val();
 
-  newNestedForm = '<input class="form-control duplicatable_nested_form"' +
-  ' type="text" value="" name="player[notes_attributes][2][note]" id="player_notes_attributes_2_note">' +
-  '<input type="hidden" value="5" name="player[notes_attributes][2][id]" id="player_notes_attributes_2_id">'
+  // update various html fields based on new number of player notes
+  numNote = $('.duplicatable_nested_form').length;
 
 
+  // $.ajax({
+  //       url: 'notes',
+  //       type: 'POST',
+  //       data: {"note":
+  //         {player_id: player_id, owner_id: owner_id} },
+  //       dataType: 'json',
+  //       success: function(data, textStatus, jqXHR)
+  //     {
+            // need to set value for newly created note's hidden field
+  //         noteId = data.id
 
+            // newNote = newNestedForm(numNote, noteId);
+            //
+            // if (numNote == 0) {
+            //   $('.notes-container').append(newNote);
+            // }
+            // else {
+            //   lastNestedForm = $('.duplicatable_nested_form').last();
+            //   $( newNote ).insertAfter( lastNestedForm );
+            // }
+
+  //     },
+  //     error: function (jqXHR, textStatus, errorThrown)
+  //     {
   //
+  //     }
+  //     });
 
-
-  // nestedForm = $('.duplicatable_nested_form').last().clone();
-  lastNestedForm = $('.duplicatable_nested_form').last();
-  // newNestedForm  = $(nestedForm).clone();
-  $( newNestedForm ).insertAfter( lastNestedForm );
 });
 
 // $("#update-submit").on('click', function(){
@@ -72,6 +92,13 @@ $("#add-note-btn").on('click', function(e) {
 // //     // });
 // //
 // });
+function newNestedForm(numNote, noteId){
+  newNote = '<input class="form-control duplicatable_nested_form"' +
+  ' type="text" value="" name="player[notes_attributes]['+ numNote +'][note]" id="player_notes_attributes_'+numNote+'_note">' +
+  '<input type="hidden" value="'+noteId+'" name="player[notes_attributes]['+numNote+'][id]" id="player_notes_attributes_'+numNote+'_id">'
+
+  return newNote;
+};
 
 
 });
