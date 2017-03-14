c719c92bc917d24655fb07129e5e69e7c7f2639a
mattschreiber <matt.schreiber01@gmail.com>
2017-03-14 13:00:07 -0400

    updated watchlist to only show people that are available, added link to player from teamprojections

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,6 +4,7 @@ class Note < ActiveRecord::Base
 
   def self.watchlist(options={})
     owner_id = options[:owner_id] || 5 # defaults to my team
-    watch_list = Note.where(owner_id: owner_id, watch: true)
+    available = options[:avail] || true #not sure if eventually want to show people even if they are on another team.
+    watch_list = Note.joins(:player).where('notes.owner_id = ? AND notes.watch = ? AND players.avail = ?', owner_id, true, available)
   end
 end
