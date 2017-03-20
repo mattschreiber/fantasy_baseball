b885eea86918de7503c0b29a611777344c66a390
mattschreiber <matt.schreiber01@gmail.com>
2017-03-20 15:17:57 -0400

    added fip and adp to pitchings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315142752) do
+ActiveRecord::Schema.define(version: 20170320190155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(version: 20170315142752) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "mlbteam_id"
+    t.float    "adp"
+    t.float    "fip"
   end
 
   add_index "pitchings", ["mlbteam_id"], name: "index_pitchings_on_mlbteam_id", using: :btree
